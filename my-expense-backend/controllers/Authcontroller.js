962b118316fde40c9196bc55b7e4f1ea955c6fc4
shaikat17 <shaikatpal56@gmail.com>
2025-05-15 23:05:40 +0600

    Login Functionality added.

---
+++ my-expense-backend/controllers/Authcontroller.js
@@ -0,0 +1,70 @@
+import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
+import generateToken from '../utils/generateToken.js';
+
+
+export const registerUser = async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+  
+      // Check if user already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+  
+      // Hash password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+  
+      // Create and save user
+      const user = new User({
+        name,
+        email,
+        password: hashedPassword,
+      });
+  
+      await user.save();
+  
+      // Generate JWT
+      const token = generateToken(user._id);
+  
+      res.status(201).json({
+        message: 'User registered successfully',
+        token,
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+      });
+    } catch (err) {
+      console.error('Signup error:', err.message);
+      res.status(500).json({ message: 'Server error' });
+    }
+  }
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: 'Invalid credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: 'Invalid credentials' });
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
